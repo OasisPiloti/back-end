8630cd00a91f1d5d6b27e506b96dd6a0bd63a72f
geunhokinn <gghh1220@gmail.com>
2024-08-17 19:09:43 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/oasis/piloti/PilotiApplication.java
@@ -0,0 +1,13 @@
+package oasis.piloti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilotiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oasis/piloti/PilotiApplicationTests.java
@@ -0,0 +1,13 @@
+package oasis.piloti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
