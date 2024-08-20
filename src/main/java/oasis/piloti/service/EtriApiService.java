dce36ec66f731841616af7460805b93f70edf1e1
geunhokinn <gghh1220@gmail.com>
2024-08-20 17:07:53 +0900

    Feat: PronunciationController, PronunciationService, EtriApiService 작성
    - PronunciationController.java, PronunciationService.java
    - 발음 평가 요청을 처리하기 위해 컨트롤러, 서비스 작성
    
    - EtriApiService.java
    - etri api 를 호출하기 위해 EtriApiService 작성

---
+++ src/main/java/oasis/piloti/service/EtriApiService.java
@@ -0,0 +1,72 @@
+package oasis.piloti.service;
+
+import lombok.RequiredArgsConstructor;
+import oasis.piloti.dto.EtriApiResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EtriApiService {
+
+    private static final String ETRI_PRONUNCIATION_API_URL = "http://aiopen.etri.re.kr:8000/WiseASR/Pronunciation";
+
+    // 환경 변수로 등록한 ETRI API 키
+    @Value("${etri.rest.api.key}")
+    private String etriApiKey;
+
+    // ETRI API 호출을 위한 클라이언트 모듈
+    private final RestTemplate restTemplate;
+
+    public EtriApiResponse.PronunciationEvaluationDTO requestPronunciationEvaluation(String script, byte[] audioData) {
+
+        // ETRI API의 URI 가져오기
+        URI uri = UriComponentsBuilder.fromHttpUrl(ETRI_PRONUNCIATION_API_URL).build().encode().toUri();
+
+        // HTTP 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, etriApiKey);
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+
+        // 요청 본문 데이터 생성
+        Map<String, Object> requestBody = new HashMap<>();
+        Map<String, String> argument = new HashMap<>();
+
+        String audioBase64 = Base64.getEncoder().encodeToString(audioData);
+        argument.put("language_code", "korean");
+        argument.put("script", script);
+        argument.put("audio", audioBase64);
+        requestBody.put("argument", argument);
+
+        // 요청 엔터티 생성
+        HttpEntity<Map<String, Object>> mapHttpEntity = new HttpEntity<>(requestBody, headers);
+
+        // ETRI API 호출 및 응답 받기
+        EtriApiResponse.PronunciationEvaluationDTO response = restTemplate.exchange(
+                uri,
+                HttpMethod.POST,
+                mapHttpEntity,
+                EtriApiResponse.PronunciationEvaluationDTO.class
+        ).getBody();
+
+        // 응답이 null인 경우 예외 처리
+        if (response == null) {
+            throw new RuntimeException("ETRI API 서버에 문제가 있습니다.");
+        }
+
+        // 응답 반환
+        return response;
+    }
+}
