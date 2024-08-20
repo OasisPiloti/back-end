eca89c5dc5f7f5ac535620f9df4a9d7cf27b9447
geunhokinn <gghh1220@gmail.com>
2024-08-20 16:36:36 +0900

    Feat: StudyCardRepository, WordRepository, DialogueRepository 작성

---
+++ src/main/java/oasis/piloti/repository/WordRepository.java
@@ -0,0 +1,9 @@
+package oasis.piloti.repository;
+
+import oasis.piloti.entity.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WordRepository extends JpaRepository<Word, Long> {
+}
